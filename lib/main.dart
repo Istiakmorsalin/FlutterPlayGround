bc266000cf86056648286deec5b6c556243b1786
Istiak Morsalin <anik.cse@gmail.com>
2020-04-07 01:04:11 +0600

    added state full  new widget

--- lib/main.dart
+++ lib/main.dart
@@ -3,20 +3,30 @@ import 'strings.dart';
 
 void main() => runApp(GHFlutterApp());
 
-
 class GHFlutterApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: Strings.appTitle,
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text(Strings.appTitle),
-        ),
-        body: Center(
-          child: Text(Strings.appTitle),
-        ),
-      ),
+      home: GHFlutter(),
     );
   }
+}
+
+
+class GHFlutterState extends State<GHFlutter> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(Strings.appTitle),
+      ),
+      body: Text(Strings.appTitle),
+    );
+  }
+}
+
+class GHFlutter extends StatefulWidget {
+  @override
+  createState() => GHFlutterState();
 }
